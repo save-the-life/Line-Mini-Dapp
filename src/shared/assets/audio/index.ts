fce21619cf045aeb044f6c4023c5bb8a216e38a5
JungHo-K1m <qopqop55@naver.com>
2025-01-17 23:44:41 +0900

    add audio index file

---
+++ src/shared/assets/audio/index.ts
@@ -0,0 +1,31 @@
+// import bgm from "./Background.mp3"
+// import roll_dice from "./Roll_Dice.mp3"
+// import island from "./Island.mp3"
+// import rps_slot from "./RPS_slot.mp3"
+// import rps_win from "./RPS_win.mp3"
+// import rps_lose from "./RPS_lose.mp3"
+// import spin_game from "./SpinGame.mp3"
+// import button_click from "./ButtonClick.mp3"
+// import move from "./Move.mp3"
+// import high_pass from "./HighPass.mp3"
+// import level_up from "./LevelUp.mp3"
+// import reward from "./Reward.mp3"
+// import random_box from "./RandomBox.mp3"
+
+const Audios = {
+    // bgm,
+    // roll_dice,
+    // island,
+    // rps_slot,
+    // rps_win,
+    // rps_lose,
+    // spin_game,
+    // button_click,
+    // move,
+    // high_pass,
+    // level_up,
+    // reward,
+    // random_box,
+}
+
+export default Audios;
