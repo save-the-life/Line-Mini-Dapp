20eba431cc5c32c4dcc2de38998f780e8ae54643
JungHo-K1m <qopqop55@naver.com>
2025-01-17 22:43:45 +0900

    renew channel Id

--- src/entities/User/api/userAuthentication.ts
+++ src/entities/User/api/userAuthentication.ts
@@ -5,7 +5,7 @@ export const userAuthenticationWithServer = async (lineIdToken: string, referral
     try {
         const lineData = {
             lineToken: lineIdToken,
-            channelId: '2006791173',
+            channelId: '2006791189',
             referralCode: referralCode
         }
         const response = await api.post('/auth/login/line', lineData);
