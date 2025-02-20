a4502b67bacba3cc354f0c166023bf68c46483e5
JungHo-K1m <qopqop55@naver.com>
2025-02-20 15:36:16 +0900

    use HashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import App from "./App";
@@ -11,9 +12,9 @@ const queryClient = new QueryClient();
 root.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <Router>
+      <HashRouter>
         <App />
-      </Router>
+      </HashRouter>
     </QueryClientProvider>
   </React.StrictMode>
 );
