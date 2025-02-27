41a634fa5f9b0e4d9dd249de3b627d3fc3a5917f
JungHo-K1m <qopqop55@naver.com>
2025-02-27 14:57:09 +0900

    Check my-asset log

--- src/entities/Asset/api/getMyAssets.ts
+++ src/entities/Asset/api/getMyAssets.ts
@@ -4,6 +4,7 @@ export const getMyAssets = async(): Promise<any> => {
     const response = await api.get('/asset');
 
     if(response.data.code === 'OK'){
+        console.log("my asset response: ", response);
         return response.data.data;
     }else{
         console.error('Unexpected response:', response);
