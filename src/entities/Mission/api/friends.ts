4390a5c41e8eca85cca63913346c3bc2dd65e41f
JungHo-K1m <qopqop55@naver.com>
2025-03-02 16:31:52 +0900

    Check ReferralCode

--- src/entities/Mission/api/friends.ts
+++ src/entities/Mission/api/friends.ts
@@ -5,6 +5,7 @@ export const getFriends = async (): Promise<any> => {
   const response = await api.get('/friends');
 
   if (response.data.code === 'OK') {
+    console.log("레퍼럴 코드 확인: ", response);
     return response.data.data;
   } else {
     console.error('Unexpected response:', response);
