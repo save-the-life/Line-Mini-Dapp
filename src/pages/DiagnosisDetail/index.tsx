23991c52e86ed8802ea6f4cd4e1d98d0627dd324
JungHo-K1m <qopqop55@naver.com>
2025-03-06 15:22:30 +0900

    Check Padding

--- src/pages/DiagnosisDetail/index.tsx
+++ src/pages/DiagnosisDetail/index.tsx
@@ -120,7 +120,7 @@ const DiagnosisDetail: React.FC = () => {
 
   return (
     <div className="relative flex flex-col items-center text-white mx-6 md:mx-28 min-h-screen">
-      <div className="flex-1 w-full overflow-y-auto pb-6">
+      <div className="flex-1 w-full overflow-y-auto pb-24"> 
         <TopTitle title={t("ai_page.Record_Details")} back={true} />
 
         {/* 이미지 표시 */}
