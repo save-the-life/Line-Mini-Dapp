fa4ea83ebf97b18cd7a6e4bcb9037dc3b40c94b1
JungHo-K1m <qopqop55@naver.com>
2025-03-10 12:06:10 +0900

    Use  whitespace-nowrap

--- src/pages/MyAssets/index.tsx
+++ src/pages/MyAssets/index.tsx
@@ -680,7 +680,7 @@ const MyAssets: React.FC = () => {
                 {/* 결제 내역 > 지갑 연결 알림 모달창 */}
                 {showHistoryModal && (
                     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 w-full">
-                        <div className="bg-white text-black p-6 rounded-lg text-center w-[70%] max-w-[550px]">
+                        <div className="bg-white text-black p-6 rounded-lg text-center w-[70%] max-w-[550px] whitespace-nowrap">
                             <p>{t("asset_page.need_wallet")}</p>
                             <button
                                 className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg"
