71ded6b456bd6596c0ec1c1846b078e8d6f34c5f
JungHo-K1m <qopqop55@naver.com>
2025-03-11 19:28:15 +0900

    Update UI

--- src/pages/ItemStore/index.tsx
+++ src/pages/ItemStore/index.tsx
@@ -178,34 +178,34 @@ const ItemStore: React.FC = () => {
     switch (itemName.toUpperCase()) {
       case "AUTO":
         return <div className="mt-1 text-center">
-                <p className="text-lg font-semibold">Dice Auto Roller</p><br />
-                <p className="text-lg font-normal">Rolls the dice automatically</p>
+                <p className="text-lg font-semibold">Dice Auto Roller</p>
+                <p className="text-base font-normal">Rolls the dice automatically</p>
               </div>;
       case "REWARD":
         return <div className="mt-1 text-center">
-                <p className="text-lg font-semibold">Rewrd Booster</p><br />
-                <p className="text-lg font-normal">Board & Spin Reward Upgrade : 5x</p>
+                <p className="text-lg font-semibold">Rewrd Booster</p>
+                <p className="text-base font-normal">Board & Spin Reward Upgrade : 5x</p>
               </div>;
       case "GOLD":
         return <div className="mt-1 text-center">
-                <p className="text-lg font-semibold">Reward Multiplier</p><br />
-                <p className="text-lg font-normal">Game Board Rewards : 30x</p><br />
-                <p className="text-lg font-semibold mt-4">Raffle Ticket Turbo</p><br />
-                <p className="text-lg font-normal">Raffle Tickets : 6x</p>
+                <p className="text-lg font-semibold">Reward Multiplier</p>
+                <p className="text-base font-normal">Game Board Rewards : 30x</p><br />
+                <p className="text-lg font-semibold mt-4">Raffle Ticket Turbo</p>
+                <p className="text-base font-normal">Raffle Tickets : 6x</p>
               </div>;
       case "SILVER":
         return <div className="mt-1 text-center">
-                <p className="text-lg font-semibold">Reward Multiplier</p><br />
-                <p className="text-lg font-normal">Game Board Rewards : 20x</p><br />
-                <p className="text-lg font-semibold mt-4">Raffle Ticket Turbo</p><br />
-                <p className="text-lg font-normal">Raffle Tickets : 4x</p>
+                <p className="text-lg font-semibold">Reward Multiplier</p>
+                <p className="text-base font-normal">Game Board Rewards : 20x</p><br />
+                <p className="text-lg font-semibold mt-4">Raffle Ticket Turbo</p>
+                <p className="text-base font-normal">Raffle Tickets : 4x</p>
               </div>;
       case "BRONZE":
         return <div className="mt-1 text-center">
-                <p className="text-lg font-semibold">Reward Multiplier</p><br />
-                <p className="text-lg font-normal">Game Board Rewards : 10x</p><br />
-                <p className="text-lg font-semibold mt-4">Raffle Ticket Turbo</p><br />
-                <p className="text-lg font-normal">Raffle Tickets : 2x</p>
+                <p className="text-lg font-semibold">Reward Multiplier</p>
+                <p className="text-base font-normal">Game Board Rewards : 10x</p><br />
+                <p className="text-lg font-semibold mt-4">Raffle Ticket Turbo</p>
+                <p className="text-base font-normal">Raffle Tickets : 2x</p>
               </div>;
       default:
         return <div>아이템에 대한 상세 설명입니다.</div>;
