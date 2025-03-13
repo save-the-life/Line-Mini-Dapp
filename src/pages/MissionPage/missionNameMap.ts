f5d0e5b11413f6241bfe0e9ab9923365f067b3ab
JungHo-K1m <qopqop55@naver.com>
2025-03-14 01:07:02 +0900

    Check Spelling

--- src/pages/MissionPage/missionNameMap.ts
+++ src/pages/MissionPage/missionNameMap.ts
@@ -3,6 +3,6 @@ export const missionNamesMap: Record<string, string> = {
   "Follow on X": "mission_page.follow_on_x",
   "Join Telegram": "mission_page.join_telegram",
   "Subscribe to Email": "mission_page.subscribe_to_email",
-  "Follow on Medium": "mission_page.follow_on_Mediumn",
+  "Follow on Medium": "mission_page.follow_on_Medium",
   "Leave a Supportive Comment on SL X": "mission_page.leave_a_supportive_comment_on_sl_x",
 };
