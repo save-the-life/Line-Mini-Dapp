1e7b697e25b398b2d9dcc38519745963e3a6b428
JungHo-K1m <qopqop55@naver.com>
2025-03-13 19:10:30 +0900

    Update Promotion UI

--- src/pages/Promotion/index.tsx
+++ src/pages/Promotion/index.tsx
@@ -12,23 +12,21 @@ const Promotion: React.FC = () => {
   }
 
   return (
-    <div className="relative">
-      <div className="bottom-10 absolute flex w-full self-center">
-          <h2 className="font-semibold text-xl text-center mt-32 mb-4">
-            Welcome! Here is your<br />
-            Dapp Portal Promotion Reward!
-          </h2>
-          <img
-            className='w-16 h-16 object-cover mb-4'
-            src={Images.Promotion}>
-          </img>
-          <button
-            className="h-14 bg-[#0147e5] text-white rounded-full w-full mx-6 opacity-100"
-            onClick={handlePromotion}
-            >
-            Claim Now
-          </button>
-        </div>
+    <div className="flex flex-col text-white mb-32 px-6 min-h-screen">
+      <h2 className="font-semibold text-xl text-center mt-32 mb-4">
+        Welcome! Here is your<br />
+        Dapp Portal Promotion Reward!
+      </h2>
+      <img
+        className='w-16 h-16 object-cover mb-4'
+        src={Images.Promotion}
+        alt="Promotion" />
+      <button
+        className="fixed h-14 bg-[#0147e5] text-white rounded-full left-6 right-6 bottom-8"
+        onClick={handlePromotion}
+      >
+        Claim Now
+      </button>
     </div>
   );
 };
