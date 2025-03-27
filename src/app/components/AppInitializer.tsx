65403c72794a2df097b4afbc3dad68dd9b6cae64
JungHo-K1m <qopqop55@naver.com>
2025-03-27 13:18:47 +0900

    Check Log

--- src/app/components/AppInitializer.tsx
+++ src/app/components/AppInitializer.tsx
@@ -97,7 +97,7 @@ const AppInitializer: React.FC<AppInitializerProps> = ({ onInitialized }) => {
             navigate("/dice-event");
           }
         } catch (error: any) {
-          // // console.error("[AppInitializer] 프로모션 수령 여부 확인 중 에러: ", error);
+          console.error("[AppInitializer] 프로모션 수령 여부 확인 중 에러: ", error);
           navigate("/dice-event");
         }
       } else {
@@ -105,16 +105,16 @@ const AppInitializer: React.FC<AppInitializerProps> = ({ onInitialized }) => {
         navigate("/dice-event");
       }
     } catch (error: any) {
-      // // console.error("[AppInitializer] getUserInfo() 중 에러:", error);
+      console.error("[AppInitializer] getUserInfo() 중 에러:", error);
 
       if (error.message === "Please choose your character first.") {
-        // // console.error("[AppInitializer] 오류: 캐릭터가 선택되지 않음 -> /choose-character 이동");
+        console.error("[AppInitializer] 오류: 캐릭터가 선택되지 않음 -> /choose-character 이동");
         navigate("/choose-character");
         return;
       }
 
       if ((error.message === "Request failed with status code 403" || error.response?.status === 403)) {
-        // // console.error("[AppInitializer] 403 에러 감지 -> 재인증 흐름 호출");
+        console.error("[AppInitializer] 403 에러 감지 -> 재인증 흐름 호출");
         // localStorage.removeItem("accessToken");
         // 재인증 흐름: handleTokenFlow() 를 호출하여 LIFF 토큰 기반 재인증 진행
         // await handleTokenFlow();
@@ -123,7 +123,7 @@ const AppInitializer: React.FC<AppInitializerProps> = ({ onInitialized }) => {
 
       // 에러 코드가 500인 경우 accessToken 삭제 후 한 번만 재시도
       if ((error.code === 500 || error.response?.status === 500) && retryCount < 1) {
-        // // console.error("[AppInitializer] 에러 코드 500 감지 -> localStorage의 accessToken 제거 후 재시도");
+        console.error("[AppInitializer] 에러 코드 500 감지 -> localStorage의 accessToken 제거 후 재시도");
         localStorage.removeItem("accessToken");
         await getUserInfo(retryCount + 1);
         return;
@@ -194,6 +194,7 @@ const AppInitializer: React.FC<AppInitializerProps> = ({ onInitialized }) => {
   
     // 정규표현식 패턴 검사
     if (referralPattern.test(referralCode)) {
+      console.log(`[AppInitializer] "${referralCode}"는 레퍼럴 코드 확인`);
       localStorage.setItem("referralCode", referralCode);
     } else {
       console.log(`[AppInitializer] "${referralCode}"는 레퍼럴 코드가 아님`);
@@ -204,54 +205,54 @@ const AppInitializer: React.FC<AppInitializerProps> = ({ onInitialized }) => {
 
   // 토큰(서버용 Access Token) 처리 및 사용자 검증
   const handleTokenFlow = async () => {
-    // // console.log("[AppInitializer] handleTokenFlow() 시작");
+    console.log("[AppInitializer] handleTokenFlow() 시작");
     const accessToken = localStorage.getItem("accessToken");
-    // // console.log("[AppInitializer] 현재 localStorage의 accessToken 확인");
+    console.log("[AppInitializer] 현재 localStorage의 accessToken 확인");
 
     if (!accessToken) {
-      // // console.log("[AppInitializer] 서버용 토큰이 없음 -> LINE 로그인 여부 확인");
+      console.log("[AppInitializer] 서버용 토큰이 없음 -> LINE 로그인 여부 확인");
       const lineToken = liff.getAccessToken();
-      // // console.log("[AppInitializer] liff.getAccessToken() 확인");
+      console.log("[AppInitializer] liff.getAccessToken() 확인");
 
       if (!lineToken) {
-        // // console.error("[AppInitializer] LINE 토큰이 없습니다. dapp 접근이 불가합니다.");
+        console.error("[AppInitializer] LINE 토큰이 없습니다. dapp 접근이 불가합니다.");
         throw new Error("LINE앱으로 로그인 후 사용바랍니다.");
       }
 
       try {
-        // // console.log("[AppInitializer] userAuthenticationWithServer() 호출");
+        console.log("[AppInitializer] userAuthenticationWithServer() 호출");
         const refCode = localStorage.getItem("referralCode");
         const isInitial = await userAuthenticationWithServer(lineToken, refCode);
 
         if (isInitial === undefined) {
-          // // console.error("[AppInitializer] 서버 인증 실패 (isInitial이 undefined)");
+          console.error("[AppInitializer] 서버 인증 실패 (isInitial이 undefined)");
           throw new Error("사용자 인증 실패");
         } else if (isInitial) {
-          // // console.log("[AppInitializer] 신규 사용자 -> /choose-character 이동");
+          console.log("[AppInitializer] 신규 사용자 -> /choose-character 이동");
           navigate("/choose-character");
         } else {
-          // // console.log("[AppInitializer] 기존 사용자 -> getUserInfo() 시도");
+          console.log("[AppInitializer] 기존 사용자 -> getUserInfo() 시도");
           await getUserInfo();
         }
       } catch (error) {
-        // // console.error("[AppInitializer] userAuthenticationWithServer() 중 에러:", error);
+        console.error("[AppInitializer] userAuthenticationWithServer() 중 에러:", error);
         throw error;
       }
     } else {
-      // // console.log("[AppInitializer] 서버용 토큰이 있음 -> getUserInfo() 시도");
+      console.log("[AppInitializer] 서버용 토큰이 있음 -> getUserInfo() 시도");
       await getUserInfo();
     }
   };
 
   // 에러 핸들링
   const handleError = (error: any, navigate: (path: string) => void) => {
-    // // console.error("[AppInitializer] 앱 초기화 중 오류:", error);
+    console.error("[AppInitializer] 앱 초기화 중 오류:", error);
     if (error.message === "Please choose your character first.") {
-      // // console.error("오류: 캐릭터가 선택되지 않음 -> /choose-character 이동");
+      console.error("오류: 캐릭터가 선택되지 않음 -> /choose-character 이동");
       navigate("/choose-character");
       return;
     } else {
-      // // console.error("[AppInitializer] 그 외 오류 발생:", error);
+      console.error("[AppInitializer] 그 외 오류 발생:", error);
       localStorage.clear();
       window.location.reload();
     }
@@ -259,12 +260,12 @@ const AppInitializer: React.FC<AppInitializerProps> = ({ onInitialized }) => {
 
   useEffect(() => {
     // localStorage.clear();
-    // // console.log("[AppInitializer] useEffect() - initializeApp() 진입");
+    console.log("[AppInitializer] useEffect() - initializeApp() 진입");
 
     // 가장 먼저 라인 브라우저 여부를 체크하여, 외부 브라우저이면 즉시 /connect-wallet으로 이동
-    // // console.log("라인브라우저 확인 : ", liff.isInClient());
+    console.log("라인브라우저 확인 : ", liff.isInClient());
     if (!liff.isInClient()) {
-      // // console.log("[AppInitializer] 외부 브라우저 접근 감지 -> /connect-wallet 이동");
+      console.log("[AppInitializer] 외부 브라우저 접근 감지 -> /connect-wallet 이동");
       navigate("/connect-wallet");
       setShowSplash(false);
       onInitialized();
@@ -273,13 +274,13 @@ const AppInitializer: React.FC<AppInitializerProps> = ({ onInitialized }) => {
 
     const initializeApp = async () => {
       if (initializedRef.current) {
-        // // console.log("[AppInitializer] 이미 초기화됨 -> 중단");
+        console.log("[AppInitializer] 이미 초기화됨 -> 중단");
         return;
       }
       initializedRef.current = true;
 
       try {
-        // // console.log("[AppInitializer] LIFF 초기화 시작");
+        console.log("[AppInitializer] LIFF 초기화 시작");
         await liff.init({
           liffId: import.meta.env.VITE_LIFF_ID,
           withLoginOnExternalBrowser: true,
@@ -290,7 +291,7 @@ const AppInitializer: React.FC<AppInitializerProps> = ({ onInitialized }) => {
         //   chainId: "1001",
         // });
 
-        // // console.log("[AppInitializer] LIFF 초기화 완료");
+        console.log("[AppInitializer] LIFF 초기화 완료");
 
         // 브라우저 언어 기반 언어 설정
         // // console.log("[AppInitializer] 브라우저 언어 기반 언어 설정 시작");
@@ -305,13 +306,13 @@ const AppInitializer: React.FC<AppInitializerProps> = ({ onInitialized }) => {
         // // console.log("전역 관리 지갑 정보 초기화 진행");
         // clearWallet();
 
-        // // console.log("[AppInitializer] handleTokenFlow() 호출");
+        console.log("[AppInitializer] handleTokenFlow() 호출");
         await handleTokenFlow();
       } catch (error) {
-        // // console.error("[AppInitializer] initializeApp() try-catch 에러:", error);
+        console.error("[AppInitializer] initializeApp() try-catch 에러:", error);
         handleError(error, navigate);
       } finally {
-        // // console.log("[AppInitializer] 초기화 최종 처리 -> 스플래시 제거 및 onInitialized() 호출");
+        console.log("[AppInitializer] 초기화 최종 처리 -> 스플래시 제거 및 onInitialized() 호출");
         setShowSplash(false);
         onInitialized();
       }
