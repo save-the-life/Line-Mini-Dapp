fde38d8ea002437e9c3a48ebc73d014069c96f5a
JungHo-K1m <qopqop55@naver.com>
2025-04-09 01:59:35 +0900

    Check Translation

--- src/pages/MissionPage/index.tsx
+++ src/pages/MissionPage/index.tsx
@@ -15,7 +15,7 @@ import {
 import { formatNumber } from "@/shared/utils/formatNumber";
 import LoadingSpinner from "@/shared/components/ui/loadingSpinner";
 import { preloadImages } from "@/shared/utils/preloadImages";
-import { useTranslation } from "react-i18next";
+import { useTranslation, Trans } from "react-i18next";
 import { useSound } from "@/shared/provider/SoundProvider";
 import Audios from "@/shared/assets/audio";
 import Attendance from "@/widgets/Attendance";
@@ -128,27 +128,40 @@ const DailyMissionCard: React.FC<DailyMissionProps> = ({ title, image, alt }) =>
 
       {/* 상세 텍스트 영역 */}
       <div className="flex flex-col text-center item-center gap-2">
-        {/* 1) +10,000 Starpoints */}
+        {/* 1) +10,000 Star Points 메시지 */}
         <div>
           <span className="font-normal text-sm mr-1">1)</span>
-          <span className="font-semibold text-base text-[#FDE047]">+10,000 Starpoints</span><br/>
-          <span className="font-normal text-sm">for both invitees and friends</span>
+          <Trans i18nKey="mission_page.starpoints_message">
+            <span className="font-semibold text-base text-[#FDE047]">
+              +10,000 Star Points
+            </span>
+            <br />
+            <span className="font-normal text-sm">
+              for both invitees and friends
+            </span>
+          </Trans>
         </div>
 
-        {/* 2) 10% Payback */}
-        <div>
+         {/* 2) 10% Payback 메시지 */}
+         <div>
           <span className="font-normal text-sm mr-1">2)</span>
-          <span className="font-semibold text-base text-[#FDE047] mr-1">10% Payback</span>
-          <span className="font-normal text-sm">on Your Friend&apos;s Purchase</span>
+          <Trans i18nKey="mission_page.payback_message">
+            <span className="font-semibold text-base text-[#FDE047] mr-1">
+              10% Payback
+            </span>
+            <span className="font-normal text-sm">
+              on Your Friend’s Purchase
+            </span>
+          </Trans>
         </div>
 
         {/* NOTE 영역 */}
         <div className="flex items-center justify-center gap-1 mt-2">
           <img src={Images.Note} alt="Note" className="w-5 h-5 object-cover" />
-          <p className="text-sm font-semibold text-[#FDE047]">NOTE</p>
+          <p className="text-sm font-semibold text-[#FDE047]">{t("mission_page.note")}</p>
         </div>
         <p className="text-xs font-normal text-center">
-          Only users who have completed at least one game round (dice roll) will be considered valid and eligible for benefits.
+          {t("mission_page.only_user")}
         </p>
       </div>
     </div>
