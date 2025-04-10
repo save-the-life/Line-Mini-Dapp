f1bcd6a16bcceb82495809ee3ef3c51dfadf1f54
JungHo-K1m <qopqop55@naver.com>
2025-04-11 00:29:46 +0900

    Update Maintenance Page

--- src/app/components/AppInitializer.tsx
+++ src/app/components/AppInitializer.tsx
@@ -353,7 +353,8 @@ const AppInitializer: React.FC<AppInitializerProps> = ({ onInitialized }) => {
             setShowMaintenance(true);
           } else {
             console.log("[InitializeApp] 정상 초기화 완료, onInitialized() 호출");
-            onInitialized();
+            // onInitialized();
+            setShowMaintenance(true);
           }
         }
       }
@@ -368,9 +369,9 @@ const AppInitializer: React.FC<AppInitializerProps> = ({ onInitialized }) => {
   if (showSplash) {
     return <SplashScreen />;
   }
-  // if (showMaintenance) {
-  //   return <MaintenanceScreen />;
-  // }
+  if (showMaintenance) {
+    return <MaintenanceScreen />;
+  }
   return null;
 };
 
