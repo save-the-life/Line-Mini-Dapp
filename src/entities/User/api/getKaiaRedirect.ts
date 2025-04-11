b6a13ebbfe2c5d3b83ba9a815ec768872481eba0
JungHo-K1m <qopqop55@naver.com>
2025-04-11 14:52:35 +0900

    Remove console log

--- src/entities/User/api/getKaiaRedirect.ts
+++ src/entities/User/api/getKaiaRedirect.ts
@@ -5,10 +5,10 @@ export const getKaiaRedirection = async (): Promise<any> => {
   const response = await api.get('/kaia/redirect');
 
   if (response.data.code === 'OK') {
-    console.log("KAIA Reward 진입 여부: ", response);
+    // console.log("KAIA Reward 진입 여부: ", response);
     return true;
   } else {
-    console.error('Unexpected response:', response);
+    // console.error('Unexpected response:', response);
     throw new Error(response.data.message || 'Failed to fetch reward information');
   }
 };
