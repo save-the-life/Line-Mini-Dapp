1b725e2c417724fadc1aec368c10aeb66b2a1a72
JungHo-K1m <qopqop55@naver.com>
2025-05-02 16:07:39 +0900

    Remove Select Reward

--- src/pages/PreviousRaffle/index.tsx
+++ src/pages/PreviousRaffle/index.tsx
@@ -136,12 +136,12 @@ const PreviousRaffle: React.FC = () => {
     <div className="flex flex-col mb-44 text-white items-center w-full min-h-screen">
       <TopTitle title={t("reward_page.last_month")} className="px-6" back={true} />
 
-      <RewardSelectionDialog
+      {/* <RewardSelectionDialog
         open={rewardDialogOpen}
         onClose={() => setRewardDialogOpen(false)}
         data={selectedMyData}
         onSelect={(type) => handleSelectRewardType(type)}
-      />
+      /> */}
 
       <RaffleSection
               myRankings={(raffleMyRankings ?? []).map((r) => ({
