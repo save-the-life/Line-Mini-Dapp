d99c12d9628525d0396d6e6918a4846b874156db
JungHo-K1m <qopqop55@naver.com>
2025-05-04 22:58:09 +0900

    Check Maintenance

--- src/app/components/AppInitializer.tsx
+++ src/app/components/AppInitializer.tsx
@@ -323,8 +323,8 @@ const AppInitializer: React.FC<AppInitializerProps> = ({ onInitialized }) => {
           navigate("/connect-wallet");
           setShowSplash(false);
           // setShowMaintenance(true);
-          // onInitialized();
-          setShowMaintenance(true);
+          onInitialized();
+          // setShowMaintenance(true);
           return;
         }
 
@@ -367,8 +367,8 @@ const AppInitializer: React.FC<AppInitializerProps> = ({ onInitialized }) => {
             setShowMaintenance(true);
           } else {
             console.log("[InitializeApp] 정상 초기화 완료, onInitialized() 호출");
-            // onInitialized();
-            setShowMaintenance(true);
+            onInitialized();
+            // setShowMaintenance(true);
           }
         }
       }
@@ -383,9 +383,9 @@ const AppInitializer: React.FC<AppInitializerProps> = ({ onInitialized }) => {
   if (showSplash) {
     return <SplashScreen />;
   }
-  if (showMaintenance) {
-    return <MaintenanceScreen />;
-  }
+  // if (showMaintenance) {
+  //   return <MaintenanceScreen />;
+  // }
   return null;
 };
 
