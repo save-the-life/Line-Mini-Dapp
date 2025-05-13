bd9920a83a4d697238dd421bffeb0aa8012123c4
JungHo-K1m <qopqop55@naver.com>
2025-05-13 13:56:07 +0900

    Update KaiaMission

--- src/entities/Mission/api/kaiaMission.ts
+++ src/entities/Mission/api/kaiaMission.ts
@@ -1,31 +1,13 @@
-// import api from '@/shared/api/axiosInstance';
-
-// // Level2 KAIA 보상 api 
-// export const requestKaiaMission = async(signedTx:string, walletAddress: string): Promise<any> => {
-//     const info = {
-//         userSignedTx: signedTx,
-//         walletAddress: walletAddress
-//     }
-
-//     const response = await api.post("/mission/kaia", info);
-
-//     if(response.data.code === "OK"){
-//         console.log("kaia mission response: ", response);
-//         return response.data;
-//     }else {
-//         console.error('Unexpected response:', response);
-//         throw new Error(response.data.message || 'Failed to fetch get Kaia Mission');
-//     }
-// };
-
-// export default requestKaiaMission;
-
 import api from '@/shared/api/axiosInstance';
 
 // Level2 KAIA 보상 api 
-export const requestKaiaMission = async(walletAddress: string): Promise<any> => {
+export const requestKaiaMission = async(signedTx:string, walletAddress: string): Promise<any> => {
+    const info = {
+        userSignedTx: signedTx,
+        walletAddress: walletAddress
+    }
 
-    const response = await api.post("/mission/kaia", {walletAddress});
+    const response = await api.post("/mission/kaia", info);
 
     if(response.data.code === "OK"){
         console.log("kaia mission response: ", response);
