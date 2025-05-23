def19ba372e70bbb8f671fc4e5fd5dc2b834b0cb
JungHo-K1m <qopqop55@naver.com>
2025-05-23 10:42:01 +0900

    Remove SL token UI

--- src/pages/AIMenu/index.tsx
+++ src/pages/AIMenu/index.tsx
@@ -49,24 +49,24 @@ const AIMenu: React.FC = () => {
   const [slToken, setSlToken] = useState(0);
   const [loading, setLoading] = useState(true);
 
-  useEffect(()=>{
-    const getMyTokenCount = async () => {
-      try{
-        const count = await getMyslToken();
+  // useEffect(()=>{
+  //   const getMyTokenCount = async () => {
+  //     try{
+  //       const count = await getMyslToken();
   
-        if(count){
-          setSlToken(count.slCount);
-        }else{
-          // console.warn('Failed to fetch sl token count');
-        }
-      }catch(error:any){
-        // console.error('Failed to fetch sl token count:', error);
-      }finally {
-        setLoading(false); // 로딩 종료
-      }
-    };
-    getMyTokenCount();
-  }, []);
+  //       if(count){
+  //         setSlToken(count.slCount);
+  //       }else{
+  //         // console.warn('Failed to fetch sl token count');
+  //       }
+  //     }catch(error:any){
+  //       // console.error('Failed to fetch sl token count:', error);
+  //     }finally {
+  //       setLoading(false); // 로딩 종료
+  //     }
+  //   };
+  //   getMyTokenCount();
+  // }, []);
 
   // 모달 초기 상태를 LocalStorage 확인 후 설정
   const [showModal, setShowModal] = useState(() => {
@@ -109,14 +109,14 @@ const AIMenu: React.FC = () => {
 
   return (
     <div className="flex flex-col text-white mx-6 md:mx-28 min-h-screen">
-      <div className="flex items-center w-full mt-8 relative">
+      {/* <div className="flex items-center w-full mt-8 relative">
         <img
           src={Images.SLToken}
           alt="Star"
           className="w-6 h-6 mr-2"
           />
         <p className="text-lg font-semibold mr-2">{slToken} SL</p>
-      </div>
+      </div> */}
 
       <div className="grid grid-cols-2 gap-3 mt-6">
         {/* 실사진 진단 */}
@@ -149,7 +149,7 @@ const AIMenu: React.FC = () => {
 
 
       {/* SL토큰 소요 알림 모달창 */}
-      {showModal && (
+      {/* {showModal && (
         <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 w-full">
             <div className="bg-white text-black p-6 rounded-lg text-center w-[70%] max-w-[550px]">
                 <p>{t("ai_page.5SL_tokens")}</p>
@@ -161,7 +161,7 @@ const AIMenu: React.FC = () => {
                 </button>
             </div>
         </div>
-      )}
+      )} */}
     </div>
   );
 };
