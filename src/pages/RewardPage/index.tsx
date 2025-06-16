f15f52c667ee25896cc08141358a9cbca4c4ade0
JungHo-K1m <qopqop55@naver.com>
2025-06-16 10:00:11 +0900

    Update Next Ranking Reward

--- src/pages/RewardPage/index.tsx
+++ src/pages/RewardPage/index.tsx
@@ -37,7 +37,7 @@ const Reward: React.FC = () => {
 
   // 날짜 포맷 및 번역
   const dateFormat = t("date_format_md");
-  const event2 = new Date(2025, 5, 15);
+  const event2 = new Date(2025, 8, 15);
   const event3 = new Date(2025, 6, 15);
   const event4 = new Date(2025, 7, 15);
 
@@ -131,26 +131,6 @@ const Reward: React.FC = () => {
             flex flex-col items-center justify-center
             p-2
           ">
-            <p className="text-xs font-normal text-white text-center">{t("reward_page.three")} {t("reward_page.this_month")}</p>
-            <p className="text-xs font-normal text-white text-center">{t("reward_page.ranking_rewards")}</p>
-            <img 
-              className="w-4 h-4"
-              src={Images.RedTriangle}
-              alt="Red Triangle"
-            />
-            <p className="text-xs font-normal text-white text-center">{t("reward_page.ends_on")}</p>
-            <p className="text-base font-semibold text-white text-center">{moment(event2).format(dateFormat)}</p>
-          </div>
-
-          {/* 두 번째 박스 */}
-          <div className="
-            w-[110px] h-[126px] 
-            bg-gradient-to-b from-[#484ADA] to-[#2D2774]
-            rounded-3xl 
-            border-2 border-yellow-400 border-blink
-            flex flex-col items-center justify-center
-            p-2
-          ">
             <p className="text-xs font-normal text-white text-center">{t("reward_page.four")} {t("reward_page.this_month")}</p>
             <p className="text-xs font-normal text-white text-center">{t("reward_page.ranking_rewards")}</p>
             <img 
@@ -161,8 +141,8 @@ const Reward: React.FC = () => {
             <p className="text-xs font-normal text-white text-center">{t("reward_page.ends_on")}</p>
             <p className="text-base font-semibold text-white text-center">{moment(event3).format(dateFormat)}</p>
           </div>
-          
-          {/* 세 번째 박스 */}
+
+          {/* 두 번째 박스 */}
           <div className="
             w-[110px] h-[126px] 
             bg-gradient-to-b from-[#484ADA] to-[#2D2774]
@@ -180,6 +160,26 @@ const Reward: React.FC = () => {
             />
             <p className="text-xs font-normal text-white text-center">{t("reward_page.ends_on")}</p>
             <p className="text-base font-semibold text-white text-center">{moment(event4).format(dateFormat)}</p>
+          </div>
+          
+          {/* 세 번째 박스 */}
+          <div className="
+            w-[110px] h-[126px] 
+            bg-gradient-to-b from-[#484ADA] to-[#2D2774]
+            rounded-3xl 
+            border-2 border-yellow-400 border-blink
+            flex flex-col items-center justify-center
+            p-2
+          ">
+            <p className="text-xs font-normal text-white text-center">{t("reward_page.six")} {t("reward_page.this_month")}</p>
+            <p className="text-xs font-normal text-white text-center">{t("reward_page.ranking_rewards")}</p>
+            <img 
+              className="w-4 h-4"
+              src={Images.RedTriangle}
+              alt="Red Triangle"
+            />
+            <p className="text-xs font-normal text-white text-center">{t("reward_page.ends_on")}</p>
+            <p className="text-base font-semibold text-white text-center">{moment(event2).format(dateFormat)}</p>
           </div>
           
           {/* 빈 영역 */}
