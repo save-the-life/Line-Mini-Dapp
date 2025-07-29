b789304555eab2b4d96ff17a1f70e7d07136d3fc
JungHo-K1m <qopqop55@naver.com>
2025-07-29 11:09:13 +0900

    Check Tiles

--- src/features/DiceEvent/components/Tiles.tsx
+++ src/features/DiceEvent/components/Tiles.tsx
@@ -1,24 +1,28 @@
-import React from "react";
-import Images from "@/shared/assets/images";
+import React from 'react';
+import Images from '@/shared/assets/images';
 
 const StarTile: React.FC<{ count: number }> = ({ count }) => (
   <div className="flex flex-col gap-1 items-center">
-    <img src={Images.Star} alt="star" className="h-4 w-4" />
-    <p className="text-xs font-bold text-white drop-shadow">x {count}</p>
+    <img src={Images.Star} alt="star" className="h-4 w-4 max-w-full max-h-full" />
+    <p className="text-xs">x {count}</p>
   </div>
 );
 
 const DiceTile: React.FC<{ count: number }> = ({ count }) => (
   <div className="flex flex-col gap-1 items-center">
-    <img src={Images.Dice} alt="dice" className="h-4 w-4" />
-    <p className="text-xs font-bold text-white drop-shadow">x {count}</p>
+    <img src={Images.Dice} alt="dice" className="h-4 w-4 max-w-full max-h-full" />
+    <p className="text-xs">x {count}</p>
   </div>
 );
 
 const AirplaneTile: React.FC<{ text: string }> = ({ text }) => (
   <div className="flex flex-col gap-1 items-center">
-    <img src={Images.Airplane} alt="airplane" className="h-4 w-4" />
-    <p className="text-xs font-bold text-white drop-shadow">{text}</p>
+    <img
+      src={Images.Airplane}
+      alt="airplane"
+      className="h-4 w-4 max-w-full max-h-full"
+    />
+    <p className="text-xs">{text}</p>
   </div>
 );
 
