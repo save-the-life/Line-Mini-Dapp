1f5b3961789fe869d2f55e30d967fe93d800973e
JungHo-K1m <qopqop55@naver.com>
2025-07-30 14:20:28 +0900

    Update Text of Tiles

--- src/features/DiceEvent/components/Tiles.tsx
+++ src/features/DiceEvent/components/Tiles.tsx
@@ -1,28 +1,58 @@
-import React from 'react';
-import Images from '@/shared/assets/images';
+import React from "react";
+import Images from "@/shared/assets/images";
 
 const StarTile: React.FC<{ count: number }> = ({ count }) => (
-  <div className="flex flex-col gap-1 items-center">
+  <div className="flex flex-col gap-0 items-center">
     <img src={Images.StarpointIcon} alt="star" className="h-[44px] w-[44px]" />
-    <p className="text-xs">x {count}</p>
+    <p
+      className="text-xs border border-[#2A294E]"
+      style={{
+        fontFamily: "'ONE Mobile POP OTF', sans-serif",
+        fontSize: "12px",
+        fontWeight: 400,
+        color: "#FFFFFF",
+      }}
+    >
+      x {count}
+    </p>
   </div>
 );
 
 const DiceTile: React.FC<{ count: number }> = ({ count }) => (
-  <div className="flex flex-col gap-1 items-center">
+  <div className="flex flex-col gap-0 items-center">
     <img src={Images.DiceIcon} alt="dice" className="h-[44px] w-[44px]" />
-    <p className="text-xs">x {count}</p>
+    <p
+      className="text-xs border border-[#2A294E]"
+      style={{
+        fontFamily: "'ONE Mobile POP OTF', sans-serif",
+        fontSize: "12px",
+        fontWeight: 400,
+        color: "#FFFFFF",
+      }}
+    >
+      x {count}
+    </p>
   </div>
 );
 
 const AirplaneTile: React.FC<{ text: string }> = ({ text }) => (
-  <div className="flex flex-col gap-1 items-center">
+  <div className="flex flex-col gap-0 items-center">
     <img
       src={Images.AirplaneIcon}
       alt="airplane"
       className="h-[44px] w-[44px]"
     />
-    <p className="text-xs">{text}</p>
+    <p
+      className="text-xs border border-[#2A294E]"
+      style={{
+        fontFamily: "'ONE Mobile POP OTF', sans-serif",
+        fontSize: "12px",
+        fontWeight: 400,
+        color: "#FFFFFF",
+      }}
+    >
+      {text}
+    </p>
   </div>
 );
 
