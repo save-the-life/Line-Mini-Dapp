809a74bc90dc6e82ea19c7691ec1ac43bf07bd7e
JungHo-K1m <qopqop55@naver.com>
2025-08-09 16:11:08 +0900

    Check Ratio

--- src/pages/Inventory/index.tsx
+++ src/pages/Inventory/index.tsx
@@ -60,7 +60,7 @@ const Inventory: React.FC = () => {
           backgroundSize: "cover",
           backgroundPosition: "center bottom",
           width: "100%",
-          height: "45vh",
+          height: "50vh",
           minHeight: 200,
         }}
         className="w-full mx-6 flex flex-col"
@@ -90,7 +90,7 @@ const Inventory: React.FC = () => {
 
       {/* 보유 중인 아이템 목록 영역 */}
       <div
-        className="w-full h-[55vh] mx-6"
+        className="w-full h-[5vh] mx-6"
         style={{
           background: "linear-gradient(180deg, #282F4E 0%, #0044A3 100%)",
         }}
