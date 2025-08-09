c835e4af27994ef8e7ee5292ecabc87efb539871
JungHo-K1m <qopqop55@naver.com>
2025-08-09 15:14:28 +0900

    Check Height

--- src/pages/Inventory/index.tsx
+++ src/pages/Inventory/index.tsx
@@ -62,7 +62,6 @@ const Inventory: React.FC = () => {
           width: "100%",
           height: "40vh",
           minHeight: 200,
-          borderRadius: 24,
           marginBottom: 24,
         }}
         className="w-full mx-6 flex flex-col"
@@ -93,7 +92,7 @@ const Inventory: React.FC = () => {
 
       {/* 보유 중인 아이템 목록 영역 */}
       <div
-        className="w-full h-full mx-6"
+        className="w-full h-[60vh] mx-6"
         style={{
           background: "linear-gradient(180deg, #282F4E 0%, #0044A3 100%)",
           boxShadow:
