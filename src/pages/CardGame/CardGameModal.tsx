32b836e3676468151f0661e4626e1fe48802674e
JungHo-K1m <qopqop55@naver.com>
2025-08-12 15:42:02 +0900

    Update Logic

--- src/pages/CardGame/CardGameModal.tsx
+++ src/pages/CardGame/CardGameModal.tsx
@@ -279,21 +279,6 @@ const CardBettingModal = ({ myPoint, onStart, onCancel }: any) => {
                     : 1,
               }}
               disabled={!bet || parseInt(bet) <= 0 || parseInt(bet) > myPoint}
-              onClick={() => {
-                console.log("=== 베팅 버튼 직접 클릭 ===");
-                console.log("bet 값:", bet);
-                console.log("bet 타입:", typeof bet);
-                console.log("parseInt(bet):", parseInt(bet));
-                console.log("myPoint:", myPoint);
-                console.log(
-                  "버튼 disabled 조건:",
-                  !bet || parseInt(bet) <= 0 || parseInt(bet) > myPoint
-                );
-                console.log(
-                  "버튼이 비활성화됨:",
-                  !bet || parseInt(bet) <= 0 || parseInt(bet) > myPoint
-                );
-              }}
             >
               <img
                 src={Images.ButtonPointBlue}
