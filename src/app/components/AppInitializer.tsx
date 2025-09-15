c824de2cf797faecee1e75d2543299a916db44a9
JungHo-K1m <qopqop55@naver.com>
2025-09-16 00:00:04 +0900

    Remove Maintanance

--- src/app/components/AppInitializer.tsx
+++ src/app/components/AppInitializer.tsx
@@ -30,7 +30,7 @@ const AppInitializer: React.FC<AppInitializerProps> = ({ onInitialized }) => {
   const navigate = useNavigate();
   const { fetchUserData } = useUserStore();
   const [showSplash, setShowSplash] = useState(true);
-  const [showMaintenance, setShowMaintenance] = useState(true);
+  const [showMaintenance, setShowMaintenance] = useState(false);
   const initializedRef = useRef(false);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
   const is502ErrorRef = useRef(false);
@@ -369,8 +369,8 @@ const AppInitializer: React.FC<AppInitializerProps> = ({ onInitialized }) => {
           console.log("[Step 2-2] 외부 브라우저 감지 -> /connect-wallet 이동");
           navigate("/connect-wallet");
           setShowSplash(false);
-          // onInitialized();
-          setShowMaintenance(true);
+          onInitialized();
+          // setShowMaintenance(true);
           return;
         }
 
@@ -425,15 +425,15 @@ const AppInitializer: React.FC<AppInitializerProps> = ({ onInitialized }) => {
             console.log(
               "[InitializeApp] 정상 초기화 완료, onInitialized() 호출"
             );
-            // onInitialized();
-            setShowMaintenance(true);
+            onInitialized();
+            // setShowMaintenance(true);
           }
         }
       }
     };
 
     initializeApp();
-  }, [fetchUserData, navigate]);
+  }, [fetchUserData, navigate, onInitialized]);
 
   if (errorMessage) {
     return (
@@ -443,9 +443,9 @@ const AppInitializer: React.FC<AppInitializerProps> = ({ onInitialized }) => {
   if (showSplash) {
     return <SplashScreen />;
   }
-  if (showMaintenance) {
-    return <MaintenanceScreen />;
-  }
+  // if (showMaintenance) {
+  //   return <MaintenanceScreen />;
+  // }
   return null;
 };
 
