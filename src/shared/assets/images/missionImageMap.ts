17a56fed19afd1bfb8b78c17e3e8557954287221
JungHo-K1m <qopqop55@naver.com>
2025-12-24 11:44:20 +0900

    Check Mission Box Background

--- src/shared/assets/images/missionImageMap.ts
+++ src/shared/assets/images/missionImageMap.ts
@@ -5,7 +5,7 @@ import Images from './index';
 const missionImageMap: { [key: string]: { imageKey: keyof typeof Images; className: string } } = {
   'Follow on X': { imageKey: 'Twitter', className: 'follow-on-x-mission-card' },
   'Join Telegram': { imageKey: 'Telegram', className: 'join-telegram-mission-card' },
-  'Subscribe to Email': { imageKey: 'Email', className: 'subscribe-to-email-mission-card' },
+  'Subscribe Email': { imageKey: 'Email', className: 'subscribe-to-email-mission-card' },
   'Follow on Medium': { imageKey: 'Medium', className: 'follow-on-Medium-mission-card' },
   // 필요한 만큼 추가적인 미션과 이미지 매핑을 추가하세요.
 };
